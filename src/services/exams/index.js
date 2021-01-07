ab6144815c15569282d0d5055d28a7d5cceffddb
elinoza <hillcakmak@gmail.com>
2021-01-07 17:45:04 +0300

    Update index.js

--- src/services/exams/index.js
+++ src/services/exams/index.js
@@ -5,21 +5,56 @@ const { getexams, writeexams ,getquestions} = require("../../fsUtilities")
 // const { selectQuestions} = require("../../functions.js")
 const examsRouter = express.Router()
 
+const pickQuestions = (maxDuration,numberOfQuestions,length) => {
+  /**
+   *  choose question
+   *  currentDuration+=question.duration
+   *  if currentDuration is lt  maxDuration
+   *     add question to array
+   * else 
+   *    step 1
+   * 
+   */
+  let selectedQuestionsArray=[]
+  currentDuration=0;
+  questions = []
+  while(currentDuration<maxDuration){
+    let randomIndex=Math.floor(Math.random() * length)
+    let selectedQuestion=questions[randomIndex]
+    
+    if(currentDuration+selectedQuestion.duration<maxDuration){
+      selectedQuestionsArray.push(selectedQuestion)
+      currentDuration+=selectedQuestion.duration
+    }
+    else{
+      if(selectedQuestionsArray===numberOfQuestions)
+
+      break;
+
+
+    }
+    
+
+  }
+}
+
 examsRouter.post("/start",async (req,res,next)=> {
 try{
   const exams = await getexams()
   const questions = await getquestions()
-  let selectedQuestionsArray=[]
+  //let selectedQuestionsArray=[]
   const TotalDuration=300
-  let duration= 0
-  for (let i=0; i<5; i++){
-    let randomIndex=Math.floor(Math.random() * questions.length)
-    selectedQuestion=questions[randomIndex]
-    duration+= selectedQuestion.duration 
-    if (duration >TotalDuration){ i -= 1 }
-    else{selectedQuestionsArray.push(selectedQuestion)}
+  // let duration= 0
+pickQuestions(TotalDuration, 5,questions.length)
+
+  // for (let i=0; i<5; i++){
+  //   let randomIndex=Math.floor(Math.random() * questions.length)
+  //   selectedQuestion=questions[randomIndex]
+  //   duration+= selectedQuestion.duration 
+  //   if (duration >TotalDuration){ i -= 1 }
+  //   else{selectedQuestionsArray.push(selectedQuestion)}
     
-  }
+  // }
   
 const response={
   ...req.body,
