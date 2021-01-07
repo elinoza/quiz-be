bbc3f25ac3f47b17cf03b1130b7986fcedc96a87
elinoza <hillcakmak@gmail.com>
2021-01-07 20:06:31 +0300

    gchj

--- src/services/exams/index.js
+++ src/services/exams/index.js
@@ -2,59 +2,49 @@ const express = require("express")
 const uniqid = require("uniqid")
 const { join } = require("path")
 const { getexams, writeexams ,getquestions} = require("../../fsUtilities")
-// const { selectQuestions} = require("../../functions.js")
+
+
 const examsRouter = express.Router()
 
-const pickQuestions = (maxDuration,numberOfQuestions,length) => {
-  /**
-   *  choose question
-   *  currentDuration+=question.duration
-   *  if currentDuration is lt  maxDuration
-   *     add question to array
-   * else 
-   *    step 1
-   * 
-   */
-  let selectedQuestionsArray=[]
-  currentDuration=0;
-  questions = []
-  while(currentDuration<maxDuration){
-    let randomIndex=Math.floor(Math.random() * length)
-    let selectedQuestion=questions[randomIndex]
-    
-    if(currentDuration+selectedQuestion.duration<maxDuration){
-      selectedQuestionsArray.push(selectedQuestion)
-      currentDuration+=selectedQuestion.duration
-    }
-    else{
-      if(selectedQuestionsArray===numberOfQuestions)
-
-      break;
-
-
-    }
-    
-
-  }
-}
 
 examsRouter.post("/start",async (req,res,next)=> {
 try{
   const exams = await getexams()
   const questions = await getquestions()
-  //let selectedQuestionsArray=[]
-  const TotalDuration=300
-  // let duration= 0
-pickQuestions(TotalDuration, 5,questions.length)
-
-  // for (let i=0; i<5; i++){
+  let selectedQuestionsArray=[]
+  const TotalDuration=900
+   let duration= 0
+   
+  //  for (let i=0; i<5; i++){
   //   let randomIndex=Math.floor(Math.random() * questions.length)
   //   selectedQuestion=questions[randomIndex]
   //   duration+= selectedQuestion.duration 
-  //   if (duration >TotalDuration){ i -= 1 }
-  //   else{selectedQuestionsArray.push(selectedQuestion)}
-    
+  // selectedQuestionsArray.push(selectedQuestion)
   // }
+  
+while (selectedQuestionsArray.length<6) { 
+ let randomIndex=Math.floor(Math.random() * questions.length)
+    selectedQuestion=questions[randomIndex]
+    duration+= selectedQuestion.duration 
+    if (duration < TotalDuration)  {selectedQuestionsArray.push(selectedQuestion)}
+ 
+}
+  //let i=0;
+  //  while(duration < 300){
+  //   let randomIndex=Math.floor(Math.random() * questions.length)
+  //   let selectedQuestion=questions[randomIndex]
+    
+  //   if(duration+selectedQuestion.duration<300){
+  //     selectedQuestionsArray.push(selectedQuestion)
+  //     console.log(duration, selectedQuestion.duration)
+  //     duration += selectedQuestion.duration 
+  //     i=i+1
+  //   }
+  //   else{
+  //     if(i=>5){break}
+      
+  //   }
+  
   
 const response={
   ...req.body,
@@ -66,12 +56,11 @@ const response={
   totalDuration:TotalDuration,
   questions: selectedQuestionsArray
 }
-
-
     exams.push(response)
     await writeexams(exams)
     res.status(201).send(response)
-  }
+  
+}
   catch(error){
   console.log(error)
   const err = new Error("An error occurred ")
